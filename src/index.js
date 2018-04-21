4e42c6c5cdb22214625e02746816fc4512de6b84
rohith-reddy <rohith.reddy@nykaa.com>
2018-04-21 16:10:25 +0530

    #1

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ const Home = require('./client/components/Home').default;
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
-  res.send(content);
+
+  const html = `
+  	<html>
+  		<head></head>
+  		<body>
+  			<div>${content}</div>
+  			<script src="bundle.js"></script>
+  		</body>
+  	</html>
+  `;
+  res.send(html);
 });
 
 app.listen(3000, (req, res) => {
